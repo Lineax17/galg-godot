fe33bb55de630e9ce12db99555de1c8ab45f40ac
Lineax17 <144595200+Lineax17@users.noreply.github.com>
2025-02-10 23:00:57 +0100

    adding auto self destruction after 5 seconds to bullet

--- game/scripts/bullet.gd
+++ game/scripts/bullet.gd
@@ -2,10 +2,20 @@ extends Area2D
 
 const DIRECTION : Vector2 = Vector2 (0,-1)
 
-@export var SPEED : float = 400.0
+@export var speed : float = 400.0
+@export var time_to_live : int = 5
+
+var timer = 0.0
+
 
 func _physics_process(delta: float) -> void:
-	position = position + (DIRECTION * delta * SPEED)
+	timer = timer + delta
+	
+	position = position + (DIRECTION * delta * speed)
+	
+	#destruction of bullet
+	if(timer > time_to_live):
+		self.queue_free()
 	
 func init(position: Vector2) -> void:
 	self.position = position
