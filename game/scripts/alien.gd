cc234d6a016adca34a1376b3a7b921eed308e0c0
Lineax17 <philipp.hartmann04@gmail.com>
2025-07-20 12:06:36 +0200

    add state based movement

--- game/scripts/alien.gd
+++ game/scripts/alien.gd
@@ -1,7 +1,47 @@
 extends CharacterBody2D
 
+# speed
 @export var movement_speed : float = 100.0
+@export var side_speed : float = 50.0
+# cooldown
+@export var down_time : float = 3.0
+@export var pause_time : float = 1.0  
+@export var side_time : float = 2.0  
+
+var sidespeed_direction = "right"
+var state = "down"
+var timer = 0.0
 
 func _physics_process(delta):
-	velocity = Vector2(0, movement_speed)
+	timer += delta
+
+	if state == "down":
+		velocity = Vector2(0, movement_speed)
+		if timer >= down_time:  
+			state = "pause"
+			timer = 0.0
+
+	elif state == "pause":
+		velocity = Vector2.ZERO
+		if timer >= pause_time:
+			state = "side"
+			randomize_side_direction()
+			timer = 0.0
+
+	elif state == "side":
+		if (sidespeed_direction == "left"):
+			velocity = Vector2(side_speed * -1, 0)
+		else:
+			velocity = Vector2(side_speed, 0)
+		if timer >= side_time:
+			state = "down"
+			timer = 0.0
+
 	move_and_slide()
+	
+	
+func randomize_side_direction():
+	if (randi() % 2 == 0):
+		sidespeed_direction = "left"
+	else:
+		sidespeed_direction = "right"
