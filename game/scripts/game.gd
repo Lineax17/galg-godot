c0897ba1da1ef68feb65be3312d97acec835fd44
Lineax17 <philipp.hartmann04@gmail.com>
2025-08-15 11:48:54 +0200

    fix level loading

--- game/scripts/game.gd
+++ game/scripts/game.gd
@@ -1,4 +1,25 @@
 extends Node2D
+	
+const LEVELDIRECTORY = "res://levels/"
+
+var current_level: Node = null
+var current_level_number: int = 1
 
 func _ready() -> void:
-	LevelLoader.load_level("level1.tscn")
+	load_level("level1.tscn")
+
+func load_level(filename: String) -> void:
+	# Remove old level if exists
+	if current_level and is_instance_valid(current_level):
+		current_level.queue_free()
+
+	# Load new level
+	var scene: PackedScene = load(LEVELDIRECTORY + filename)
+	if scene:
+		var scene_instance = scene.instantiate()
+		current_level = scene_instance
+		get_tree().root.get_node("Game").add_child(scene_instance)
+		PathLookup.get_player().position = Vector2(0,0)
+		PathLookup.get_camera().position = Vector2(0,0)
+	else:
+		push_error("Level konnte nicht geladen werden: " + filename)
--- game/scripts/global/path_lookup.gd
+++ game/scripts/global/path_lookup.gd
@@ -1,6 +1,10 @@
 extends Node
 
 var absolute_player_path : String = "Game/AutoScrollCamera/Player"
+var absolute_camera_path : String = "Game/AutoScrollCamera"
 
 func get_player():
-	return get_node("../" + absolute_player_path)
+	return get_node("../" + absolute_player_path) as CharacterBody2D
+
+func get_camera():
+	return get_node("../" + absolute_camera_path) as Camera2D
